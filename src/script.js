66f2ce6d89f1c7886ee76c462c2fb84ae03cb442
David <David.Piruzashvili@gmail.com>
2023-09-22 15:27:13 +0400

    cycle and conditions

--- src/script.js
+++ src/script.js
@@ -10,10 +10,33 @@ const personalMovieDB = {
     privat: false,
 };
 
-const firstQ = prompt("One of the last movies I watched", "");
- personalMovieDB.movies[firstQ] =  prompt("Rate it", "");
+if (personalMovieDB.count >= 1 && personalMovieDB.count < 10) {
+    console.log('quite a few films');
+}else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('classic viewer');
+}else if (personalMovieDB.count > 30) {
+    console.log('you are a film buff');
+}else {
+    console.log('error');
+}
 
- const secondQ = prompt("One of the last movies I watched", ""); 
- personalMovieDB.movies[secondQ] =  prompt("Rate it", "");
+
+// for (let i = 0; i < 2; i++) {
+
+//     const questions = prompt("One of the last movies I watched", ""),
+//           rate = prompt("Rate it", "");
+//     if (questions == null || questions.length > 6 || questions == '' || rate == null || rate == '') {
+//        i--;
+//     }else {
+//         personalMovieDB.movies[questions] =  rate;
+//     }
+
+// }
+
+// const firstQ = prompt("One of the last movies I watched", "");
+//  personalMovieDB.movies[firstQ] =  prompt("Rate it", "");
+
+//  const secondQ = prompt("One of the last movies I watched", ""); 
+//  personalMovieDB.movies[secondQ] =  prompt("Rate it", "");
 
 console.log(JSON.stringify(personalMovieDB, null, 4));
